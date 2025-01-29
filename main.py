25fa1c4e4d2ba39efe8b053a22ed85cc5051e8c9
jack <98613949+jkprcl@users.noreply.github.com>
2025-01-28 23:00:06 -0600

    initial logic and function first draft of some initial logic and functions
    todo:
      - change approach to creating a hidden main window
      - hide, modify, and then show the window to achieve the desired effect

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import asyncio
 import json
 from datetime import datetime
 import pathlib
+import toga
+from toga.style import Pack
 
 test_notification_file = "notification.json"
 
@@ -27,7 +30,7 @@ class Notification:
         self.expiry_time = expiry_time
 
 
-def load_notification(notification_file_path: str):
+def load_notification(notification_file_path: pathlib.Path) -> Notification | None:
     assert pathlib.Path(notification_file_path).is_file(), "File does not exist"
     with open(notification_file_path, "r") as notification_file:
         json_data = json.load(notification_file)
@@ -53,7 +56,7 @@ def load_notification(notification_file_path: str):
     return notification
 
 
-def save_notification(notification_file_path: str, notification: Notification):
+def save_notification(notification_file_path: pathlib.Path, notification: Notification):
     if not notification:
         raise ValueError("Notification cannot be empty")
     if not isinstance(notification, Notification):
@@ -65,3 +68,44 @@ def save_notification(notification_file_path: str, notification: Notification):
     except Exception as e:
         print(f"Error serializing notification: {e}")
         return
+
+
+async def show_popup(app: toga.App, notification: Notification):
+    if notification:
+        app.main_window.dialog(
+            toga.InfoDialog(title=notification.title, message=notification.message)
+        )
+
+
+async def notification_checker(notification_file_path: pathlib.Path, app: toga.App):
+    while True:
+        if notification_file_path.exists():
+            try:
+                notification = load_notification(notification_file_path)
+                if notification:
+                    asyncio.create_task(show_popup(app, notification))
+                notification_file_path.unlink()
+            except Exception as e:
+                print(f"Error loading notification: {e}")
+        await asyncio.sleep(5)
+
+
+class NotificationApp(toga.App):
+    async def app_startup(self):
+        self.main_window = toga.MainWindow(title=self.formal_name)
+        label = toga.Label(text="Notification Checker", style=Pack(padding=10))
+        self.main_window.content = label
+        self.main_window.show()
+        notification_filepath = pathlib.Path(test_notification_file)
+        asyncio.create_task(notification_checker(notification_filepath, self))
+
+
+# run the toga app
+def main():
+    return NotificationApp(
+        formal_name="Notification Checker", app_id="com.purcell-labs.notification"
+    )
+
+
+if __name__ == "__main__":
+    main().main_loop()
