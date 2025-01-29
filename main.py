f4880c38f0f9d4af12b9ff2722f0c22aecd7b476
jack <98613949+jkprcl@users.noreply.github.com>
2025-01-29 07:26:11 -0600

    rebuild
    start of a rebuild to move from the info dialogue to just managing one window

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import json
 from datetime import datetime
 import pathlib
 import toga
-from toga.style import Pack
 
 test_notification_file = "notification.json"
 
@@ -70,42 +69,27 @@ def save_notification(notification_file_path: pathlib.Path, notification: Notifi
         return
 
 
-async def show_popup(app: toga.App, notification: Notification):
-    if notification:
-        app.main_window.dialog(
-            toga.InfoDialog(title=notification.title, message=notification.message)
-        )
-
-
 async def notification_checker(notification_file_path: pathlib.Path, app: toga.App):
     while True:
         if notification_file_path.exists():
             try:
                 notification = load_notification(notification_file_path)
                 if notification:
-                    asyncio.create_task(show_popup(app, notification))
+                    print(f"Loaded notification: {notification}")
                 notification_file_path.unlink()
             except Exception as e:
                 print(f"Error loading notification: {e}")
         await asyncio.sleep(5)
 
+def build(app):
+    box = toga.Box()
+    title = "Notifier"
+    message = "Message"
+    label = toga.Label(text=message)
+    box.add(label)
 
-class NotificationApp(toga.App):
-    async def app_startup(self):
-        self.main_window = toga.MainWindow(title=self.formal_name)
-        label = toga.Label(text="Notification Checker", style=Pack(padding=10))
-        self.main_window.content = label
-        self.main_window.show()
-        notification_filepath = pathlib.Path(test_notification_file)
-        asyncio.create_task(notification_checker(notification_filepath, self))
-
+    return box
 
 # run the toga app
-def main():
-    return NotificationApp(
-        formal_name="Notification Checker", app_id="com.purcell-labs.notification"
-    )
-
-
 if __name__ == "__main__":
-    main().main_loop()
+
